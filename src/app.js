2dbeae32ba785c47d077af1c0fa176bff7ea9bd1
Marinamb19 <150665821+Marinamb19@users.noreply.github.com>
2024-02-22 18:54:30 +0000

    cartas-aleatorias-marinamb

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(randomIcons());
+  document.querySelector(".card").innerHTML = randomNumber();
+};
+
+let randomNumber = () => {
+  let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+  let indexNum = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNum];
+};
+
+let randomIcons = () => {
+  let icons = ["diamond", "spade", "heart", "club"];
+  let indexIcons = Math.floor(Math.random() * icons.length);
+  return icons[indexIcons];
 };
